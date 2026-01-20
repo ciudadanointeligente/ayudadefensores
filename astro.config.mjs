34514c55ed8fe4bca073202be9112805b821a169
pedregalux <felipe.perry@vivaldi.net>
2026-01-20 13:59:58 -0300

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from "@astrojs/alpinejs";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
   integrations: [alpinejs()],
+  adapter: cloudflare(),
 });
