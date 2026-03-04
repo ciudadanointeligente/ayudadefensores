9c77e694ce828980caca48810b535fd3adb28cc5
pedregalux <felipe.perry@vivaldi.net>
2026-03-04 11:19:00 -0300

    030326-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [alpinejs()],
+  integrations: [alpinejs({ entrypoint: '/src/entrypoint' })],
   adapter: cloudflare(),
   redirects: {
     "/": "/es/",
